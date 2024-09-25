f01386ee568b7ff92082e4d327f10aee277b6890
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-09-25 23:16:31 +0700

    v0.2 Добавлена обработка кнопок и третий фрагмент

--- app/src/main/java/com/example/study_of_oriented_graph/SecondFragment.kt
+++ app/src/main/java/com/example/study_of_oriented_graph/SecondFragment.kt
@@ -5,18 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.study_of_oriented_graph.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,11 +28,23 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//
-//        binding.buttonSecond.setOnClickListener {
-//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-//        }
+
+        for (i in 5..24) {
+            val buttonId = resources.getIdentifier("button$i", "id", requireContext().packageName)
+            val button = view.findViewById<Button>(buttonId)
+            button?.setOnClickListener {
+                val buttonText = button.text.toString()
+                navigateToThirdFragment(buttonText)
+            }
+        }
     }
+
+    private fun navigateToThirdFragment(buttonText: String) {
+        val bundle = Bundle()
+        bundle.putString("buttonText", buttonText)
+        findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment, bundle)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/study_of_oriented_graph/ThirdFragment.kt
+++ app/src/main/java/com/example/study_of_oriented_graph/ThirdFragment.kt
@@ -1,16 +1,15 @@
 package com.example.study_of_oriented_graph
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.study_of_oriented_graph.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.example.study_of_oriented_graph.databinding.FragmentThirdBinding
 
-class FirstFragment : Fragment() {
+class ThirdFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentThirdBinding? = null
 
     private val binding get() = _binding!!
 
@@ -19,7 +18,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -27,9 +26,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        val buttonText = arguments?.getString("buttonText")
+        binding.textView.text = "Выбрано $buttonText вершин"
+
     }
 
     override fun onDestroyView() {
