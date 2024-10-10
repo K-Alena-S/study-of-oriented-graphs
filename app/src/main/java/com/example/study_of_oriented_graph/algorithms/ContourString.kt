648594c728bc38eca3f39c2e58bcb0628b109383
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-10-10 18:51:15 +0700

    v0.6.2 Добавлен вывод контуров на экран

--- app/src/main/java/com/example/study_of_oriented_graph/algorithms/ContourString.kt
+++ app/src/main/java/com/example/study_of_oriented_graph/algorithms/ContourString.kt
@@ -1,9 +1,8 @@
 package com.example.study_of_oriented_graph.algorithms
 
-class ContourString(private val graphArray: Array<IntArray>) {
+class ContourString(private val graphArray: Array<IntArray>, private val contourEnd: Int) {
     private var id: String = ""
     private val m: Int = 3
-    private val contourCount: Int = 12
     private val contour: IntArray = IntArray(graphArray.size - m + 1)
 
     init {
@@ -19,6 +18,20 @@ class ContourString(private val graphArray: Array<IntArray>) {
         return id
     }
 
+    fun getArray(contourBegin: Int, contourEnd: Int): IntArray {
+        // Создаем новый массив нужного размера
+        val size = contourEnd - contourBegin + 1
+        val result = IntArray(size)
+
+        // Копируем элементы из contour в result
+        for (i in 0 until size) {
+            result[i] = contour[i + contourBegin - m]
+        }
+
+        return result
+    }
+
+
     private fun varRecursive(graphArray: Array<IntArray>, contour: IntArray, vertex: IntArray, a: Int, n: Int, k: Int) {
         if (a == k) {
             contour[n - m] += 1
@@ -30,7 +43,7 @@ class ContourString(private val graphArray: Array<IntArray>) {
                     if (graphArray[updatedK][i] == 1) {
                         var newN = n + 1
                         vertex[i] += 1
-                        if (newN <= contourCount) {
+                        if (newN <= contourEnd) {
                             varRecursive(graphArray, contour, vertex, a, newN, i)
                         }
                         newN -= 1
--- app/src/main/java/com/example/study_of_oriented_graph/interfaces/ContourFragment.kt
+++ app/src/main/java/com/example/study_of_oriented_graph/interfaces/ContourFragment.kt
@@ -1,7 +1,50 @@
 package com.example.study_of_oriented_graph.interfaces
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.study_of_oriented_graph.R
+import com.example.study_of_oriented_graph.algorithms.ContourString
 
-class ContourFragment: Fragment() {
+class ContourFragment : Fragment() {
+
+    private lateinit var contourTextView: TextView
+    private lateinit var matrix: Array<IntArray>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contour, container, false)
+    }
+
+    // В методе onViewCreated
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        contourTextView = view.findViewById(R.id.textCont)
+
+        val matrixString = arguments?.getString("matrixString").orEmpty()
+        val contourBegin = arguments?.getInt("seekBar1") ?: 4 // Задать значение по умолчанию
+        val contourEnd = arguments?.getInt("seekBar2") ?: 5
+
+        val matrix = matrixToArrayInt(matrixString)
+
+        val cs = ContourString(matrix, contourEnd)
+        val contour = cs.getArray(contourBegin, contourEnd)
+
+//        // Преобразуем IntArray в строку для отображения
+        val contourString = contour.joinToString(", ")
+        contourTextView.text = "от $contourBegin до $contourEnd\n $contourString"
+    }
+
+    private fun matrixToArrayInt(matrixString: String): Array<IntArray> {
+        return matrixString.trim().lines().map { line ->
+            line.split(", ").map { it.toInt() }.toIntArray()
+        }.toTypedArray()
+    }
 
 }
